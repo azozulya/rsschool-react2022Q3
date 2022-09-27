9fe2549b4d307720fe5033dddd92dfd17048d5e3
azozulya <bbbom@mail.ru>
2022-09-27 20:27:58 +0300

    feat: add form page, rename search form -> create user form

--- rs-react/src/App.tsx
+++ rs-react/src/App.tsx
@@ -4,6 +4,7 @@ import { Layout } from './components/Layout/Layout';
 import Main from './pages/MainPage';
 import NotFound from './pages/NotFoundPage';
 import About from './pages/AboutPage';
+import FormPage from './pages/FormPage/FormPage';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
           <Route path="about" element={<About />} />
-          <Route path="search-form" element={<SearchForm />} />
+          <Route path="search-form" element={<FormPage />} />
           <Route path="404" element={<NotFound />} />
           <Route path="*" element={<Navigate replace to={'404'} />} />
         </Route>
