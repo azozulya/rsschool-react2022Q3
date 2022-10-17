f38f0ee2e0faa02ac1d9dcb4be1a4d8d6e80fd24
azozulya <bbbom@mail.ru>
2022-10-17 22:38:48 +0300

    feat: add runtime format function and currency format

--- rs-react/src/utils/utils.tsx
+++ rs-react/src/utils/utils.tsx
@@ -5,3 +5,17 @@ export const dateFormat = (date: string) => {
 export const getYear = (date: string) => {
   return new Date(date).getFullYear();
 };
+
+export const currencyFormat = (currency: number) => {
+  return new Intl.NumberFormat('en-En', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0,
+  }).format(currency);
+};
+
+export const runtimeFormat = (time: number) => {
+  const hours = Math.trunc(time / 60);
+  const minuts = time - hours * 60;
+  return `${hours}h ${minuts}min`;
+};
