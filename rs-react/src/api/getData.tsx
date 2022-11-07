f02d9dad44584cf1b7af353ccc8fc41797bc46b6
azozulya <bbbom@mail.ru>
2022-11-08 00:04:58 +0200

    feat: change api, flickr now

--- rs-react/src/api/getData.tsx
+++ rs-react/src/api/getData.tsx
@@ -1,12 +1,23 @@
 import { TCards } from '../components/Cards/types';
 import { API_URL } from '../utils/constants';
 
-async function getData(queryString: string, page = 1): Promise<TCards | null> {
+type TPhotoResponse = {
+  photos: TCards;
+};
+
+async function getData(
+  queryString: string,
+  page = 1,
+  perpage = '100',
+  sort = 'date-posted-desc'
+): Promise<TCards | null> {
   try {
-    return await fetch(`${API_URL}&query=${queryString}&page=${page}`)
-      .then((response) => response.json() as Promise<TCards>)
-      .then((data: TCards) => {
-        return data;
+    return await fetch(
+      `${API_URL}&text=${queryString}&page=${page}&per_page=${perpage}&sort=${sort}&media=photos&license=4`
+    )
+      .then((response) => response.json() as Promise<TPhotoResponse>)
+      .then((data: TPhotoResponse) => {
+        return data.photos;
       })
       .catch((error) => {
         console.log('getData error1: ', error);
