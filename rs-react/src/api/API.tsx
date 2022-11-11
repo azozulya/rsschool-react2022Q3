ef100931b9661be769720a9c96222e65286d8ec6
azozulya <bbbom@mail.ru>
2022-11-11 11:03:58 +0200

    refactor: api response go to separate file from store

---
+++ rs-react/src/api/API.tsx
@@ -0,0 +1,19 @@
+import { TCards } from '../components/Cards/types';
+import { API_URL } from '../utils/constants';
+
+const API = {
+  getPhotos: async (searchString: string, page: number, perpage: string, sort: string) => {
+    const response = await fetch(
+      `${API_URL}&text=${searchString}&page=${page}&per_page=${perpage}&sort=${sort}&media=photo&license=4`
+    );
+
+    if (!response.ok) {
+      throw new Error("Can't load photos");
+    }
+
+    const photosData = (await response.json()) as { photos: TCards };
+    return photosData.photos as TCards;
+  },
+};
+
+export default API;
