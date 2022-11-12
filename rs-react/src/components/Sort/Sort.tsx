1ae370f68311bf0d7595303654246a68b5df61e5
azozulya <bbbom@mail.ru>
2022-11-12 15:03:57 +0200

    fix: add redable sort items

--- rs-react/src/components/Sort/Sort.tsx
+++ rs-react/src/components/Sort/Sort.tsx
@@ -1,8 +1,36 @@
 import React, { RefObject, useRef } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hook';
 import { changeItemsPerPage, sortItems } from '../../store/thunks';
-import { PER_PAGE, SORT_PARAMS } from '../../utils/constants';
 import style from './Sort.module.css';
+
+const PER_PAGE = [5, 10, 20, 30, 40, 60];
+
+const SORT_PARAMS = [
+  {
+    key: 'date-posted-desc',
+    value: 'Date posted ↑',
+  },
+  {
+    key: 'date-posted-asc',
+    value: 'Date posted ↓',
+  },
+  {
+    key: 'date-taken-asc',
+    value: 'Date taken ↓',
+  },
+  {
+    key: 'date-taken-desc',
+    value: 'Date taken ↑',
+  },
+  {
+    key: 'interestingness-desc',
+    value: 'Interestingness ↑',
+  },
+  {
+    key: 'interestingness-asc',
+    value: 'Interestingness ↓',
+  },
+];
 
 function Sort() {
   const dispatch = useAppDispatch();
@@ -35,9 +63,9 @@ function Sort() {
       <div className={style.sort}>
         <span className={style.title}>sort</span>
         <select name="sort" value={sort} onChange={onSortChangeHandler} ref={sortRef}>
-          {SORT_PARAMS.map((item) => (
-            <option key={self.crypto.randomUUID()} value={item}>
-              {item}
+          {SORT_PARAMS.map(({ key, value }) => (
+            <option key={self.crypto.randomUUID()} value={key}>
+              {value}
             </option>
           ))}
         </select>
