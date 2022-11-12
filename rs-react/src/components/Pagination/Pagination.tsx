c2b031bf54551e4a6a227bd2cab2814b7dba60d3
azozulya <bbbom@mail.ru>
2022-11-12 19:23:38 +0200

    refactor: change pagination view

--- rs-react/src/components/Pagination/Pagination.tsx
+++ rs-react/src/components/Pagination/Pagination.tsx
@@ -10,29 +10,37 @@ interface IProps {
 const Pagination = (props: IProps) => {
   const { page } = useGlobalState();
   const { setCurrentPage, goNextPage, goPrevPage } = useDispatch();
+  const { total } = props;
+  const prev = (
+    <li className={style.page} onClick={goPrevPage}>
+      {'<<'}
+    </li>
+  );
+  const next = (
+    <li className={style.page} onClick={goNextPage}>
+      {'>>'}
+    </li>
+  );
 
-  if (page === 1 && props.total === 1)
+  const pageItem = (pageNum: number) => {
     return (
-      <div>
-        <ul className={style.pagination}>
-          <li className={style.current}>1</li>
-        </ul>
-      </div>
+      <li
+        key={self.crypto.randomUUID()}
+        className={page === pageNum ? style.current : style.page}
+        onClick={() => setCurrentPage(pageNum)}
+      >
+        {pageNum}
+      </li>
     );
+  };
 
-  if (page === 1) {
+  if (total <= 10) {
     return (
       <div>
         <ul className={style.pagination}>
-          <li className={style.page}>{'<<'}</li>
-          <li className={style.current}>1</li>
-          <li className={style.page}>...</li>
-          <li className={style.page} onClick={() => setCurrentPage(props.total)}>
-            {props.total}
-          </li>
-          <li className={style.page} onClick={goNextPage}>
-            {'>>'}
-          </li>
+          {page > 1 && prev}
+          {new Array(total).fill(0).map((_, idx) => pageItem(idx + 1))}
+          {page < total && next}
         </ul>
       </div>
     );
@@ -41,39 +49,29 @@ const Pagination = (props: IProps) => {
   return (
     <div>
       <ul className={style.pagination}>
-        {page < props.total && page > 1 && (
-          <>
-            <li className={style.page} onClick={goPrevPage}>
-              {'<<'}
-            </li>
-            <li className={style.page} onClick={() => setCurrentPage(1)}>
-              1
-            </li>
-            <li className={style.page}>...</li>
-            <li className={style.current}>{page}</li>
-            <li className={style.page}>...</li>
-            <li className={style.page} onClick={() => setCurrentPage(props.total)}>
-              {props.total}
-            </li>
-            <li className={style.page} onClick={goNextPage}>
-              {'>>'}
-            </li>
-          </>
-        )}
-
-        {page === props.total && (
-          <>
-            <li className={style.page} onClick={goPrevPage}>
-              {'<<'}
-            </li>
-            <li className={style.page} onClick={() => setCurrentPage(1)}>
-              1
-            </li>
-            <li className={style.page}>...</li>
-            <li className={style.current}>{props.total}</li>
-            <li className={style.page}>{'>>'}</li>
-          </>
-        )}
+        <>
+          {page > 1 && prev}
+          {page <= 5 && new Array(5).fill(0).map((_, idx) => pageItem(idx + 1))}
+          {page > 5 && (
+            <>
+              <li className={style.page} onClick={() => setCurrentPage(1)}>
+                1
+              </li>
+              <li className={style.page}>...</li>
+            </>
+          )}
+          {page > 5 && page < total - 4 && [page - 1, page, page + 1].map((num) => pageItem(num))}
+          {page < total - 4 && (
+            <>
+              <li className={style.page}>...</li>
+              <li className={style.page} onClick={() => setCurrentPage(total)}>
+                {total}
+              </li>
+            </>
+          )}
+          {page > total - 5 && new Array(5).fill(0).map((_, idx) => pageItem(total - 4 + idx))}
+          {page < total && next}
+        </>
       </ul>
     </div>
   );
