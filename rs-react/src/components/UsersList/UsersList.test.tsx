c3f3c331bb36270eabb4178108fa042a7c93fc9c
azozulya <bbbom@mail.ru>
2022-11-12 10:56:10 +0200

    fix: delete props, change to fc

--- rs-react/src/components/UsersList/UsersList.test.tsx
+++ rs-react/src/components/UsersList/UsersList.test.tsx
@@ -4,15 +4,15 @@ import { render, screen } from '@testing-library/react';
 import { usersData } from '../../assets/testData/testUsers';
 import { TUserCard } from '../CreateForm/CreateForm.types';
 
-const getUsers = () => {
-  return new Promise<TUserCard[]>((resolve) => {
-    setTimeout(() => resolve(usersData), 300);
-  });
-};
+// const getUsers = () => {
+//   return new Promise<TUserCard[]>((resolve) => {
+//     setTimeout(() => resolve(usersData), 300);
+//   });
+// };
 
 describe('User list', () => {
   test('renders empty user list', () => {
-    render(<UsersList users={[]} />);
+    render(<UsersList />);
 
     const usersList = screen.getByTestId('usersList');
     expect(usersList).toBeInTheDocument();
@@ -20,8 +20,8 @@ describe('User list', () => {
   });
 
   test('renders user list', async () => {
-    const users = await getUsers();
-    render(<UsersList users={users} />);
+    //const users = await getUsers();
+    render(<UsersList />);
 
     const avatars = screen.getAllByRole('img');
     expect(avatars).toBeDefined();
--- rs-react/src/components/UsersList/UsersList.tsx
+++ rs-react/src/components/UsersList/UsersList.tsx
@@ -1,48 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useAppSelector } from '../../store/hook';
 import { TUserCard } from '../CreateForm/CreateForm.types';
 import style from './UsersList.module.css';
 
-type TProps = {
-  users: TUserCard[];
+const createUserCard = (user: TUserCard) => {
+  const { id, username, birthday, country, gender, avatar } = user;
+
+  return (
+    <div key={id} className={style.user}>
+      {avatar && <img src={avatar} className={style.userAvatar} />}
+      <ul className={style.userContent}>
+        <li className={style.userOption}>
+          <span className={style.label}>Name:</span> {username}
+        </li>
+        <li className={style.userOption}>
+          <span className={style.label}>Gender:</span> {gender}
+        </li>
+        <li className={style.userOption}>
+          <span className={style.label}>Birthday:</span>
+          {new Intl.DateTimeFormat('en-UK', { dateStyle: 'medium' }).format(new Date(birthday))}
+        </li>
+        <li className={style.userOption}>
+          <span className={style.label}>Country:</span> {country}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
-export class UsersList extends Component<TProps> {
-  constructor(props: TProps) {
-    super(props);
-  }
+export const UsersList = () => {
+  const { users } = useAppSelector((state) => state.form);
 
-  createUserCard = (user: TUserCard) => {
-    const { id, username, birthday, country, gender, avatar } = user;
-    return (
-      <div key={id} className={style.user}>
-        <img src={avatar} className={style.userAvatar} />
-        <ul className={style.userContent}>
-          <li className={style.userOption}>
-            <span className={style.label}>Name:</span> {username}
-          </li>
-          <li className={style.userOption}>
-            <span className={style.label}>Gender:</span> {gender}
-          </li>
-          <li className={style.userOption}>
-            <span className={style.label}>Birthday:</span>
-            {new Intl.DateTimeFormat('en-UK', { dateStyle: 'medium' }).format(new Date(birthday))}
-          </li>
-          <li className={style.userOption}>
-            <span className={style.label}>Country:</span> {country}
-          </li>
-        </ul>
-      </div>
-    );
-  };
-
-  render() {
-    const users = this.props.users;
-    return (
-      <div className={style.users} data-testid="usersList">
-        {users.map((user) => this.createUserCard(user))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={style.users} data-testid="usersList">
+      {users.map((user) => createUserCard(user))}
+    </div>
+  );
+};
 
 export default UsersList;
