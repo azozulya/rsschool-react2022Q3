c95cff560d7d0564202b5f89f511f55de5b72b84
azozulya <bbbom@mail.ru>
2022-11-12 11:14:22 +0200

    fix: change tsx -> ts

--- rs-react/src/utils/constants.ts
+++ rs-react/src/utils/constants.ts
@@ -9,7 +9,7 @@ const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&la
 const API_POPULAR_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
 const API_IMG_URL = 'https://image.tmdb.org/t/p/w300';
-// 'https://api.themoviedb.org/3/movie/920?api_key=6fcb9620a842bea26304d7962404e1fe&language=en-US';
+
 const API_MOVIE_DETAILS_URL = 'https://api.themoviedb.org/3/movie/';
 
 export {
