fc145d6a6d6215de9353e3c261c8e007af0b559c
azozulya <bbbom@mail.ru>
2022-11-12 19:23:38 +0200

    refactor: change pagination view

--- rs-react/src/components/Pagination/Pagination.tsx
+++ rs-react/src/components/Pagination/Pagination.tsx
@@ -7,28 +7,36 @@ const Pagination = () => {
   const { page, pages } = useAppSelector((state) => state.photos);
   const dispatch = useAppDispatch();
 
-  if (page === 1 && pages === 1)
-    return (
-      <div>
-        <ul className={style.pagination}>
-          <li className={style.current}>1</li>
-        </ul>
-      </div>
-    );
+  const prev = (
+    <li className={style.page} onClick={() => dispatch(goToPrevPage())}>
+      {'<<'}
+    </li>
+  );
+  const next = (
+    <li className={style.page} onClick={() => dispatch(goToNextPage())}>
+      {'>>'}
+    </li>
+  );
 
-  if (page === 1) {
+  const pageItem = (pageNum: number) => {
+    return (
+      <li
+        key={self.crypto.randomUUID()}
+        className={page === pageNum ? style.current : style.page}
+        onClick={() => dispatch(goToPage(pageNum))}
+      >
+        {pageNum}
+      </li>
+    );
+  };
+
+  if (pages <= 10) {
     return (
       <div>
         <ul className={style.pagination}>
-          <li className={style.page}>{'<<'}</li>
-          <li className={style.current}>1</li>
-          <li className={style.page}>...</li>
-          <li className={style.page} onClick={() => dispatch(goToPage(pages))}>
-            {pages}
-          </li>
-          <li className={style.page} onClick={() => dispatch(goToNextPage())}>
-            {'>>'}
-          </li>
+          {page > 1 && prev}
+          {new Array(pages).fill(0).map((_, idx) => pageItem(idx + 1))}
+          {page < pages && next}
         </ul>
       </div>
     );
@@ -37,39 +45,25 @@ const Pagination = () => {
   return (
     <div>
       <ul className={style.pagination}>
-        {page < pages && page > 1 && (
-          <>
-            <li className={style.page} onClick={() => dispatch(goToPrevPage())}>
-              {'<<'}
-            </li>
-            <li className={style.page} onClick={() => dispatch(goToPage(1))}>
-              1
-            </li>
-            <li className={style.page}>...</li>
-            <li className={style.current}>{page}</li>
-            <li className={style.page}>...</li>
-            <li className={style.page} onClick={() => dispatch(goToPage(pages))}>
-              {pages}
-            </li>
-            <li className={style.page} onClick={() => dispatch(goToNextPage())}>
-              {'>>'}
-            </li>
-          </>
-        )}
-
-        {page === pages && (
-          <>
-            <li className={style.page} onClick={() => dispatch(goToPrevPage())}>
-              {'<<'}
-            </li>
-            <li className={style.page} onClick={() => dispatch(goToPage(1))}>
-              1
-            </li>
-            <li className={style.page}>...</li>
-            <li className={style.current}>{pages}</li>
-            <li className={style.page}>{'>>'}</li>
-          </>
-        )}
+        <>
+          {page > 1 && prev}
+          {page <= 5 && new Array(5).fill(0).map((_, idx) => pageItem(idx + 1))}
+          {page > 5 && (
+            <>
+              {pageItem(1)}
+              <li className={style.page}>...</li>
+            </>
+          )}
+          {page > 5 && page < pages - 4 && [page - 1, page, page + 1].map((num) => pageItem(num))}
+          {page < pages - 4 && (
+            <>
+              <li className={style.page}>...</li>
+              {pageItem(pages)}
+            </>
+          )}
+          {page > pages - 5 && new Array(5).fill(0).map((_, idx) => pageItem(pages - 4 + idx))}
+          {page < pages && next}
+        </>
       </ul>
     </div>
   );
